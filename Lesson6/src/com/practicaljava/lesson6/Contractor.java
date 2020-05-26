725e1acd08d40749aa29a41a78c7860846941e7c
aromansson <ggsromansson@gmail.com>
2020-05-27 00:13:22 +0300

    homework

--- Lesson6/src/com/practicaljava/lesson6/Contractor.java
+++ Lesson6/src/com/practicaljava/lesson6/Contractor.java
@@ -2,12 +2,19 @@ package com.practicaljava.lesson6;
 
 public class Contractor extends Person implements Payable {
 
-
+	public Contractor(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public boolean increasePay(int percent) {
-		// TODO Auto-generated method stub
-		return false;
+		if (percent <= INCREASE_CAP) {
+			System.out.println(" Increasing salary by " + percent + "%. " + getName());
+		} else {
+			System.out.println("Sorry, can't increase hourly rate by more than " + INCREASE_CAP + "%.");
+		}
+		return true;
 	}
 
 }
--- Lesson6/src/com/practicaljava/lesson6/Employee.java
+++ Lesson6/src/com/practicaljava/lesson6/Employee.java
@@ -1,11 +1,16 @@
 package com.practicaljava.lesson6;
 
-public class Employee implements Payable {
+public class Employee extends Person implements Payable {
+
+	public Employee(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public boolean increasePay(int percent) {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println("Increasing salary by " + percent + "%. " + getName());
+		return true;
 	}
 
 }
