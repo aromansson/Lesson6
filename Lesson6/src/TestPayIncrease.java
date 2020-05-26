4c771f531802d2eedcf8858b4e8c3c9ab733e890
ggsromansson <ggsromansson@gmail.com>
2020-05-26 19:03:17 +0300

    dfg

---
+++ Lesson6/src/TestPayIncrease.java
@@ -0,0 +1,16 @@
+import com.practicaljava.lesson6.Contractor;
+import com.practicaljava.lesson6.Employee;
+import com.practicaljava.lesson6.Person;
+
+public class TestPayIncrease {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Person workers[] = new Person[3];
+workers[0] = new Employee("John");
+workers[1] = new Contractor("Mary");
+workers[2] = new Employee("Steve");
+
+	}
+
+}
